326ab85c1437d7c8be00fbfe5abc9dbe14d0b1cd
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-04-09 12:42:42 -0400

    Answered Q2 - loop

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -6,7 +6,12 @@ const addToFrontOrBack = (arr, value, isFront) => {
   }
 };
 
-const reverseString = () => {
+const reverseString = (str) => {
+  let newStr = ""
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i]
+  }
+  return newStr
 };
 
 const newArrayFullOf = () => {
